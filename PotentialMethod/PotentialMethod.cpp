1d9465153e420deb3bd6873d776e97f521bd6c94
130120013 <alinap95@yandex.ru>
2017-12-10 00:31:39 +1000

    Skeleton

--- PotentialMethod/PotentialMethod.cpp
+++ PotentialMethod/PotentialMethod.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+	vector<double> X; //входы
+	vector<double> params; //важность
+	vector<double> H; //ширина потенциала
+	vector<double> Y; //выходы
+
+	double epsilon; //допустимая ошибка
+	int i = 0;
+
+	//erf - 2/sqrt(PI)*integral(0 - x) (e^(-t^2) dt)
+
+	while (erf(1) > epsilon) //пока функция ошибки выдает результат больше epsilon
+	{
+		
+	}
 	return 0;
 }
